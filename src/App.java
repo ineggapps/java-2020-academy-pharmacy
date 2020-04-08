aaacda795bc0621f5c5f61b4ac6ab0ec4392cf67
mijooJeon <dmsgml0386@naver.com>
2020-04-08 14:00:57 +0900

    test

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Customer customer = new Customer();
-		Pharmacist pharmacist = new Pharmacist();
+		Pharmacist pharmacist = new Pharmacist(); //¤¾¤·
 		try {
 			int ch;
 			while (true) {
